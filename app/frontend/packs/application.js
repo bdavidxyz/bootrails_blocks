04da95f62b7c2d3cad9d14f444bdefaffca9751d
David Boureau <bdavidxyz@gmail.com>
2021-03-16 16:07:59 +0100

    installed hotwire

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -5,10 +5,11 @@
 
 import '../js/bootstrap_js_files.js'  
 import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
+import "@hotwired/turbo-rails"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
 Rails.start()
-Turbolinks.start()
 ActiveStorage.start()
+
+import "controllers"
