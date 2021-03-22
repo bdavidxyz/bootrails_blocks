d088f70d33154c760cd2037347f5bc381aad6c0b
David Boureau <bdavidxyz@gmail.com>
2021-03-22 09:57:37 +0100

    bug scss

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
       resources :heroes, only: [:index, :show] 
       resources :features, only: [:index, :show] 
       resources :pricings, only: [:index, :show] 
+      resources :settings, only: [:index, :show] 
       resources :footers, only: [:index, :show] 
     end
   end
